f4fe3e70da0692e479f4f54b612f0466cf544413
Fatih Arapoglu <f3tt4n@gmail.com>
2022-12-16 03:47:57 +0300

    prettier

--- script.js
+++ script.js
@@ -1,31 +1,28 @@
-// countdown function with setinterval
+// countdown function with setInterval
 const countdownDOM = document.getElementById("countdown");
 
-let people = 95486184
+let people = 95486184;
 setInterval(() => {
     people--
     countdownDOM.innerHTML = people;
 }, 50);
 
-
 // password check
-const formDOM = document.querySelector('form');
-formDOM.addEventListener('submit' , e => {
-    const pass1DOM = document.querySelector('#password');
-    const pass2DOM = document.querySelector('#confirm-password');
-    const errorMsgDOM = document.querySelector('.error-message');
-    
+const formDOM = document.querySelector("form");
+formDOM.addEventListener("submit" , e => {
+    const pass1DOM = document.querySelector("#password");
+    const pass2DOM = document.querySelector("#confirm-password");
+    const errorMsgDOM = document.querySelector(".error-message");
     if (pass1DOM.value.length < 6){
         e.preventDefault();
-        errorMsgDOM.textContent = "*Password must be at least 6 characters."
-        errorMsgDOM.style.display = 'block';
+        errorMsgDOM.textContent = "*Password must be at least 6 characters.";
+        errorMsgDOM.style.display = "block";
     }
-
     else if (pass1DOM.value !== pass2DOM.value) {
         e.preventDefault();
-        pass1DOM.classList.add('error');
-        pass2DOM.classList.add('error');
-        errorMsgDOM.textContent = "*Passwords do not match."
-        errorMsgDOM.style.display = 'block';
+        pass1DOM.classList.add("error");
+        pass2DOM.classList.add("error");
+        errorMsgDOM.textContent = "*Passwords do not match.";
+        errorMsgDOM.style.display = "block";
     }
 });
